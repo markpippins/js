f07bf218ddcc83125a275655d9c990b050e9e5ba
Mark Pippins <mpippins@gmail.com>
2025-10-23 22:53:32 -0400

    convex-based slack clone

---
+++ frameworks/react/source/slack-clone/convex/schema.ts
@@ -0,0 +1,32 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+import { authTables } from "@convex-dev/auth/server";
+
+const applicationTables = {
+  channels: defineTable({
+    name: v.string(),
+    createdBy: v.id("users"),
+  }).index("by_name", ["name"]),
+
+  messages: defineTable({
+    channelId: v.id("channels"),
+    authorId: v.id("users"),
+    content: v.string(),
+  })
+    .index("by_channel", ["channelId"])
+    .searchIndex("search_content", {
+      searchField: "content",
+      filterFields: ["channelId"],
+    }),
+
+  profiles: defineTable({
+    userId: v.id("users"),
+    name: v.string(),
+    avatarId: v.optional(v.id("_storage")),
+  }).index("by_user", ["userId"]),
+};
+
+export default defineSchema({
+  ...authTables,
+  ...applicationTables,
+});
